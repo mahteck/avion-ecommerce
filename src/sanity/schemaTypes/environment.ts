a989a3449ad8c09f8f83fd88acb089892168b83b
Muhammad Shoaib <shoaibm@baysint.com>
2025-01-22 11:51:21 +0500

    signup and and enviormnent type added

---
+++ src/sanity/schemaTypes/environment.ts
@@ -0,0 +1,23 @@
+export default {
+    name: 'environment',
+    title: 'Environment Types',
+    type: 'document',
+    fields: [
+      {
+        name: 'name',
+        title: 'Environment Name',
+        type: 'string',
+      },
+      {
+        name: 'code',
+        title: 'Environment Code',
+        type: 'string',
+      },
+      {
+        name: 'description',
+        title: 'Description',
+        type: 'text',
+      },
+    ],
+  };
+  
--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -5,7 +5,8 @@ import customer from './customer'
 import order from './order'
 import cartitem from './cartItem'
 import user from './user'
+import environment from './environment'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [Product, Category, order, customer, cartitem, user],
+  types: [Product, Category, order, customer, cartitem, user, environment],
 }
